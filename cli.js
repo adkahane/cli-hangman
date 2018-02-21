792bbed26839407c3992ad12fdaf7312df498161
adkahane <adam.kahane.nj@gmail.com>
2018-02-20 19:05:52 -0800

    game loop working. after win or lose asks if user wants to play again working. input validation to make sure input is a character

--- cli.js
+++ cli.js
@@ -2,63 +2,95 @@
 var game = require("./game.js");
 var inquirer = require("inquirer");
 var chalk = require("chalk");
-var log = console.log;
-// Number of guesses left
-var guessLeft = 10;
+var log = console.log
 var playerGuess;
 var playGame;
-
+// Get a new game going
 function hereWeGo() {
-var newGame = new game();
-newGame.startGame();
-rounds();
+  // Number of guesses left
+  var guessLeft = 10;
+  // New instance of game object
+  var newGame = new game();
+  newGame.startGame();
+  
+  rounds();
 
-// Game recursion loop
-function rounds() {
-  console.log("\nMystery Word:");
-  console.log(newGame.wordProgress() + "\n");
-  if (guessLeft > 0){
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "guess",
-      message: "What Letter will you choose?"
-    }])
-  .then(function(answers) {
-    playerGuess = newGame.checkGuess(answers.guess);
-    if (playerGuess === false) {
-      guessLeft--;
-      console.log(newGame.wordProgress());
-      console.log("You have " + guessLeft + " guesses left.\n");
-      rounds();
+  // Start of a new round
+  function rounds() {
+    log(chalk.white("\n-----------") + chalk.red.bold("\nCLI HANGMAN\n") + chalk.white("-----------\n\nGuess this Word Dude!\n"));
+    console.log("You have " + chalk.red.bold(guessLeft) + " guesses left!\n");
+    console.log(newGame.wordProgress() + "\n");
+    if (guessLeft > 0){
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "guess",
+          message: chalk.green("What Letter will you choose?"),
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return false;
+            }
+            return true;
+          }
+        }])
+      .then(function(answers) {
+        playerGuess = newGame.checkGuess(answers.guess);
+        if (playerGuess === false) {
+          guessLeft--;
+          // After losing, ask the player if they want to play again
+          if (guessLeft === 0) {
+            log(chalk.bgRed.yellow.bold("\n           YOU LOSE           \n"));
+            inquirer.prompt([
+              {
+                type: "confirm",
+                name: "playAgain",
+                message: "Would you like to play again?"
+              }
+            ]).
+            then(function(answers) {
+              playGame = answers.playAgain;
+              if (playGame === true) {
+                hereWeGo();
+              }
+              else {
+                console.log(chalk.blue("\nGoodbye"));
+                return 0;
+              }
+            });
+          }
+          else {
+            rounds();
+          }
+        }
+        else {
+          if (newGame.wordProgress().replace(/\s+/g, '') === newGame.gameWord) {
+            log(chalk.bgRed.yellow.bold("\n           YOU WIN           \n"))
+            console.log("The word was " + chalk.cyan.bold(newGame.wordProgress()) + " !\n");
+            inquirer.prompt([
+              {
+                type: "confirm",
+                name: "playAgain",
+                message: "Would you like to play again?"
+              }
+            ]).
+            then(function(answers) {
+              playGame = answers.playAgain;
+              if (playGame === true) {
+                hereWeGo();
+              }
+              else {
+                console.log(chalk.blue("\nGoodbye"));
+                return 0;
+              }
+            });
+          }
+          else{
+            rounds();
+          }
+        }
+      });
     }
-    else {
-      console.log("Good Guess!");
-      console.log(newGame.wordProgress());
-      rounds();
-    }
-  });
-}
-}
-}
-// After losing, ask the player if they want to play again
-if (guessLeft === 0) {
-  inquirer.prompt([
-    {
-      type: "confirm",
-      name: "playAgain",
-      message: "Would you like to play again?"
-    }
-  ]).
-  then(function(answers) {
-    playGame = answers.playAgain;
-    if (playGame === true) {
-      hereWeGo();
-    }
-    else {
-      console.log("Goodbye");
-    }
-  });
+  }
 }
 
 hereWeGo();
